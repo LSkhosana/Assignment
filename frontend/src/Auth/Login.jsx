441b3badbf40625abcf1552634e0b283e3c67b8d
Lesedi <skhosanalesedi888@gmail.com>
2025-05-08 12:08:49 +0200

    update

--- frontend/src/Auth/Login.jsx
+++ frontend/src/Auth/Login.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './auth.css'
 
 const Login = () => {
   return (
     <div className='container'>
   <form>
-    <div>
+    <div className='Email'>
       <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" required />
+      <input type="email" id="email" name="email" required style={{width:"250px"}}/>
     </div>
 
-    <div>
+    <div className='Password'>
       <label htmlFor="password">Password:</label>
-      <input type="password" id="password" name="password" required />
+      <input type="password" id="password" name="password" required style={{width:"250px"}}/>
     </div>
-
+    <Link style={{ color:"black"}}>Forgot password?</Link>
     <button type="submit">Login</button>
   </form>
 </div>
